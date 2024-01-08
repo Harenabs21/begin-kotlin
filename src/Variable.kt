3e0d13a531c712c74a26f5656a81d593d110b24e
Harenabs21 <hei.harena.6@gmail.com>
2024-01-08 11:57:53 +0300

    feat: create Variable.kt for the variable exercise

---
+++ src/Variable.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val name = "Harena"
+    val age = 19
+    val school = "HEI"
+    val language = "Kotlin"
+
+    println("Hello everyone, my name is $name , I am $age years old and I'm student from $school . Today, I'm introducing myself with a programming language called: $language")
+}
