858aa2436dc76cc7181d544af4d3491021f71806
Harenabs21 <hei.harena.6@gmail.com>
2024-01-08 17:38:16 +0300

    feat: create BasicOperations.kt for the basic operations exercise

---
+++ src/BasicOperations.kt
@@ -0,0 +1,12 @@
+var a = 4
+var sum = a + 2
+var b = 3
+var minus = b - 2
+var c = 5
+var multiply = c * 5
+var d = 48
+var division =  d / 2
+
+fun main(){
+    println("sum: $sum \n subtraction: $minus \n multiplication: $multiply \n division: $division")
+}
